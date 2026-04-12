8e81e44c7611c5020582df2b499f4df53769ff4f
Murilo <murilocm2005@gmail.com>
2026-04-12 09:00:34 -0400

    calculo

--- Atividade numero 1/script.js
+++ Atividade numero 1/script.js
@@ -3,4 +3,4 @@ let n1 = Number(prompt("Digite seu primeiro número"));
 let n2 = Number(prompt("Digite seu segundo número"));
 
 let soma = Number(n1) + Number(n2);
-alert (soma);
+alert ("O resultado da soma é" +soma);
---
+++ Atividade numero 2/script.js
@@ -0,0 +1,9 @@
+alert("Seja bem vindo")
+let numero = Number(prompt("Digite o número"));
+
+let sucessor = numero + 1; //sucessor
+let antecessor = numero - 1; //antecessor
+
+alert("O numero digitado é " +numero +". Seu antecessor é " +antecessor +" e seu sucessor é " +sucessor)
+
+//alert("Antecessor: " + antecessor + "\nNúmero: " + numero + "\nSucessor: " + sucessor);

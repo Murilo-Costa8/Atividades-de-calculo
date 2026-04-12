3e15c49c5fd47fa80dec260594800e7ae0d9d2c8
Murilo <murilocm2005@gmail.com>
2026-04-12 08:40:39 -0400

    Calculo

---
+++ Atividade numero 1/script.js
@@ -0,0 +1,6 @@
+alert("Olá, seja bem-vindo");
+let n1 = Number(prompt("Digite seu primeiro número"));
+let n2 = Number(prompt("Digite seu segundo número"));
+
+let soma = Number(n1) + Number(n2);
+alert (soma);

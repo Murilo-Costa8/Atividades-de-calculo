a1be268b367edc44cf6b57156ddd8e358b6897de
Murilo <murilocm2005@gmail.com>
2026-04-12 10:42:31 -0400

    Update script.js

--- Atividade numero 9/script.js
+++ Atividade numero 9/script.js
@@ -1,5 +1,5 @@
-let n1 = Number(prompt("Qual a sua idade??"))
+let n1 = Number(prompt("Qual a sua idade??"));
 
 let media = n1 * 365
 
-alert (`Você ja viveu ${media} dias`)
+alert (`Você ja viveu ${media} dias`);

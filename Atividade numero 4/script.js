fc1f5710f09096dc26f1bbd5c1ce05c3d9bdcdda
Murilo <murilocm2005@gmail.com>
2026-04-12 09:32:05 -0400

    Reais para Dolar

---
+++ Atividade numero 4/script.js
@@ -0,0 +1,5 @@
+let n1 = Number(prompt("Digite quanto de grana tu tem:"));
+
+let media = n1 * 5
+
+alert (`Você possui R$${n1} reais e isso seria exatamente USD$${media} Doláres nos EUA, ta rico ein!!!😂🤣😂`);

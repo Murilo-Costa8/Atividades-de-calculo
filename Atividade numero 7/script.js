6e3f4046373bca0b2b4cc3776a8a11c96f9d1a83
Murilo <murilocm2005@gmail.com>
2026-04-12 10:34:37 -0400

    adicionando %

--- Atividade numero 7/script.js
+++ Atividade numero 7/script.js
@@ -4,4 +4,4 @@ let n1 = Number (prompt("Digite o preço do produto para aplicarmos o desconto."
 
 let media = n1 * 0.85
 
-alert (`O preço deste produtro possui desconto de 15% vai de R$${n1} para R$${media.toFixed(1)}`);
+alert (`O preço deste produtro possui desconto de 15% vai de R$${n1} para R$${media.toFixed(2)}`);
---
+++ Atividade numero 8/script.js
@@ -0,0 +1,5 @@
+let n1 = Number(prompt("Quanto o cliente pagou?"));
+
+let media = n1 * 1.1
+
+alert (`A conta do cliente com os 10% do garçom ficou R$${media.toFixed(2)}.`);

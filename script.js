7d953d438594886a49d2ab4093f2e5a2cc4a5f84
Murilo <murilocm2005@gmail.com>
2026-04-13 17:46:55 +0100

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let n1 = Number(prompt("Escreva quantas horas tu trabalha no mês:"))
+
+let n2 = Number(prompt("Agora escreva o quanto você recebe por hora:"))
+
+let media1 = n1 * n2
+
+alert (`Seu salário bruto é: R$${media1}.`)
+
+let media2 = media1 * 0.92
+
+alert (`E seu salário com impostos é de R$${media2}`)
